a465f2ce77e0cc1e0eb9820e463699ff8cfbc450
bjarkifannar <bjarkifs@gmail.com>
2015-11-04 08:38:18 +0000

    Added the second image to the index page
    Added the second image to the index page and two ghost buttons

--- Verkefni6/index.php
+++ Verkefni6/index.php
@@ -12,12 +12,16 @@
 		<div class="main">
 			<div class="col-1-3 floatleft index-col index-col-left"></div>
 			<div class="col-1-3 floatleft index-col index-col-center">
-				<button class="ghost-button">Buy Tickets</button>
+				<div class="index-buttons">
+					<button class="ghost-button" onclick="navigate('buy.php');">Buy Tickets</button>
+					<button class="ghost-button">Details</button>
+				</div>
 			</div>
 			<div class="col-1-3 floatleft index-col index-col-right"></div>
 		</div>
 		<?php
 			require "inc/footer.php";
 		?>
+		<script src="js/global_functions.js"></script>
 	</body>
 </html>
