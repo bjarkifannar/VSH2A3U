3785fb8177cd97b5a31e5be82e347376cc9688b9
Stimmikex <2509972569@TSNEM1267.2t.local>
2015-11-04 09:24:37 +0000

    Made buy.php
    made buy.php and added the buy list

---
+++ Verkefni6/buy.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			require "inc/head.php";
+		?>
+	</head>
+	<body>
+		<?php
+			require "inc/header.php";
+		?>
+		<div class="buy-div">
+			<ul>
+				<li class="buy-title">Normal</li>
+				<li class="buy-price">$60</li>
+				<li class="buy-button"><button class="button" href="#">Buy</button></li>
+			</ul>
+		</div>
+		<div class="buy-div">
+			<ul>
+				<li class="buy-title">VIP</li>
+				<li class="buy-price">$200</li>
+				<li class="buy-button"><button class="button" href="#">Buy</button></li>
+			</ul>
+		</div>
+		<div class="buy-div">
+			<ul>
+				<li class="buy-title">Moshpit</li>
+				<li class="buy-price">$70</li>
+				<li class="buy-button"><button class="button" href="#">Buy</button></li>
+			</ul>
+		</div>
+		<?php
+			require "inc/footer.php";
+		?>
+	</body>
+</html>
