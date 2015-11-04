36da7fc677a1a0781a123842f7883596d901a174
Stimmikex <2509972569@TSNEM1267.2t.local>
2015-11-04 09:35:41 +0000

    Added Selcet in buy.php

--- Verkefni6/buy.php
+++ Verkefni6/buy.php
@@ -13,6 +13,12 @@
 			<ul>
 				<li class="buy-title">Normal</li>
 				<li class="buy-price">$60</li>
+				<select>
+				  	<option value="buy-normal1">1</option>
+				  	<option value="buy-normal2">2</option>
+				  	<option value="buy-normal3">3</option>
+				 	<option value="buy-normal4">4</option>
+				</select>	
 				<li class="buy-button"><button class="button" href="#">Buy</button></li>
 			</ul>
 		</div>
@@ -20,6 +26,12 @@
 			<ul>
 				<li class="buy-title">VIP</li>
 				<li class="buy-price">$200</li>
+				<select>
+				  	<option value="buy-vip1">1</option>
+				  	<option value="buy-vip2">2</option>
+				  	<option value="buy-vip3">3</option>
+				 	<option value="buy-vip4">4</option>
+				</select>	
 				<li class="buy-button"><button class="button" href="#">Buy</button></li>
 			</ul>
 		</div>
@@ -27,6 +39,12 @@
 			<ul>
 				<li class="buy-title">Moshpit</li>
 				<li class="buy-price">$70</li>
+				<select>
+				  	<option value="buy-moshpit1">1</option>
+				  	<option value="buy-moshpit2">2</option>
+				  	<option value="buy-moshpit3">3</option>
+				 	<option value="buy-moshpit4">4</option>
+				</select>	
 				<li class="buy-button"><button class="button" href="#">Buy</button></li>
 			</ul>
 		</div>
