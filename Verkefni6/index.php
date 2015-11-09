9e02fdde473dc47df870d6026bdbac5b64eb470a
bjarkifannar <bjarkifs@gmail.com>
2015-11-09 17:49:02 +0000

    Changed the index page and fixed some styles

--- Verkefni6/index.php
+++ Verkefni6/index.php
@@ -10,18 +10,22 @@
 			require "inc/header.php";
 		?>
 		<div class="main">
-			<div class="col-1-3 floatleft index-col index-col-left"></div>
+			<!-- <div class="col-1-3 floatleft index-col index-col-left">
+				<img src="img/index-left.jpg">
+			</div>
 			<div class="col-1-3 floatleft index-col index-col-center">
+				<img src="img/index-center.jpg">
 				<div class="index-buttons">
 					<button class="ghost-button" onclick="navigate('buy.php');">Buy Tickets</button>
 					<button class="ghost-button" onclick="navigate('about.php');">Details</button>
 				</div>
 			</div>
-			<div class="col-1-3 floatleft index-col index-col-right"></div>
-			<div class="col-1-3 index-col index-col-left event-div">
+			<div class="col-1-3 floatleft index-col index-col-right">
+				<img src="img/index-right.jpg">
+			</div> -->
+			<div class="col-1-3 floatleft index-col index-col-left event-div">
 				<img src="img/skalmold-logo.jpg">
-				<h2 class="event-name">Event Name!</h2>
-				<h4 class="event-date">Event Date</h4>
+				<h2 class="event-name">Event Name! - Event Date</h2>
 				<div class="index-buttons">
 					<button class="ghost-button" onclick="navigate('buy.php');">Buy Tickets</button>
 					<button class="ghost-button" onclick="navigate('about.php');">Details</button>
