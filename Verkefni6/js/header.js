6c82b013fc481f7357985cc537a7bdd2a48bd1cb
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-11 09:50:07 +0000

    Changed menu to header-list

--- Verkefni6/js/header.js
+++ Verkefni6/js/header.js
@@ -1,14 +1,14 @@
 $( ".cross" ).hide();
 $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
+$( ".header-list" ).slideToggle( "slow", function() {
 $( ".hamburger" ).hide();
 $( ".cross" ).show();
 });
 });
 
 $( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
+$( ".header-list" ).slideToggle( "slow", function() {
 $( ".cross" ).hide();
 $( ".hamburger" ).show();
 });
