1b30569e978bd362aeb29829b2eeae02a9ec6fdf
bjarkifannar <bjarkifs@gmail.com>
2015-11-11 09:18:44 +0000

    Added images and changed the header and footer

--- Verkefni6/inc/header.php
+++ Verkefni6/inc/header.php
@@ -1,6 +1,6 @@
 <header class="header">
 	<ul class="header-list">
-		<li><a href="index.php" class="header-link"><img src="img/skalmold-logo.jpg"></a></li>
+		<li><a href="index.php" class="header-link"><img src="img/logo.png"></a></li>
 		<li><a href="index.php" class="header-link change-bg link-padding">Home</a></li>
 		<li><a href="about.php" class="header-link change-bg link-padding">About</a></li>
 	</ul>
