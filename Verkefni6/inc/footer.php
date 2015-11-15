358099236c7b114798640e3417a952b206df848d
bjarkifannar <bjarkifs@gmail.com>
2015-11-15 14:08:17 +0000

    Added a contact form to the footer

--- Verkefni6/inc/footer.php
+++ Verkefni6/inc/footer.php
@@ -1,7 +1,46 @@
 <footer class="footer">
 	<ul class="footer-list">
-		<li><b>Email:</b> skalmold@skalmold.is</li>
-		<li><b>General Manager:</b> stijn@edgemanagement.be | www.edgemanagement.be</li>
-		<li><b>Booking:</b> merle@dragon-productions.com | www.dragon-productions.eu</li>
+		<li><b>Email:</b> admin@dummy.com</li>
+		<li><b>Booking:</b> booking@dummy.com</li>
 	</ul>
+	<div class="contact-div">
+		<form accept-charset="UTF-8">
+			<table class="contact-table">
+				<tr>
+					<td>
+						<label for="name">Name*:</label>
+					</td>
+					<td>
+						<input type="text" name="name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="email">Email*:</label>
+					</td>
+					<td>
+						<input type="text" name="email">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="message">Message*:</label>
+					</td>
+					<td>
+						<textarea cols="25" rows="5"></textarea>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<input type="button" name="submit" value="Send">
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<p>* required field</p>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
 </footer>
