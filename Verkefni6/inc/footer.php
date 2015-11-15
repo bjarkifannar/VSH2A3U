fd1132786124a50351f3ca00e540c825473eab60
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-15 20:14:12 +0000

    Change footer

--- Verkefni6/inc/footer.php
+++ Verkefni6/inc/footer.php
@@ -1,41 +1,8 @@
-
-	<div class="contact-div">
-		<form accept-charset="UTF-8">
-			<table class="contact-table">
-				<tr>
-					<td>
-						<label for="name">Name: Tickets</label>
-					</td>
-					<td>
-						<input type="text" name="name">
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="email">Email: Tickets@gmail.com</label>
-					</td>
-					<td>
-						<input type="text" name="email">
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="message">Message: 354-820-7291</label>
-					</td>
-					<td>
-						<textarea cols="25" rows="5"></textarea>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<input type="button" name="submit" value="Send">
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<p>* required field</p>
-					</td>
-				</tr>
-			</table>
-		</form>
-	</div>
+<footer class="footer">
+	<ul class="footer-list">
+		<li><b>Name:</b>Tickets</li>
+		<li><b>Email:</b>Tickets@gmail.com</li>
+		<li><b>Phone:</b>354-820-7291</li>
+		<li><b>Heimilisfang:</b>Skólavörðuholti 101 Reykjavík</li>
+	</ul>
+</footer>
