0b22aa2b156ae69e17a84f0f728a324a05167f77
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-16 13:43:01 +0000

    Added contact and changed the footer

--- Verkefni6/inc/footer.php
+++ Verkefni6/inc/footer.php
@@ -1,45 +1,8 @@
 <footer class="footer">
 	<ul class="footer-list">
-<<<<<<< HEAD
-		<li><b>Email:</b> admin@dummy.com</li>
-		<li><b>Booking:</b> booking@dummy.com</li>
+		<li><b>Name:</b> Tickets</li>
+		<li><b>Email:</b> Tickets@gmail.com</li>
+		<li><b>Phone:</b> 354-820-7291</li>
+		<li><b>Address:</b> Skólavörðuholti 101 Reykjavík</li>
 	</ul>
-	<div class="contact-div">
-		<form accept-charset="UTF-8">
-			<table class="contact-table">
-				<tr>
-					<td>
-						<input type="text" name="name" placeholder="Name*">
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="text" name="email" placeholder="Email*">
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<textarea cols="25" rows="5" placeholder="Message*"></textarea>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="button" name="submit" value="Send">
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<p>* required field</p>
-					</td>
-				</tr>
-			</table>
-		</form>
-	</div>
-=======
-		<li><b>Name:</b>Tickets</li>
-		<li><b>Email:</b>Tickets@gmail.com</li>
-		<li><b>Phone:</b>354-820-7291</li>
-		<li><b>Heimilisfang:</b>Skólavörðuholti 101 Reykjavík</li>
-	</ul>
->>>>>>> origin/master
 </footer>
